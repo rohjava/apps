c3cc30ca4d16a11db8c212dbce16f9727d3b4408
rohjava <rohit.govindgari@gmail.com>
2024-03-04 21:29:38 -0500

    Commiting the changes

---
+++ src/test/java/com/apps/SpringbootPractise/SpringbootPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps.SpringbootPractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
